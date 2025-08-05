cdbc481fa474f0197ed36db8bc38e3ad12b80e38
Dixon <510672083@qq.com>
2025-08-05 18:15:24 +0800

    测试

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<img src="./assets/logo.png" alt="logo">
+		<SchoolName></SchoolName>
+		<StudentName></StudentName>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	//引入组件
+	import SchoolName from './components/SchoolName'
+	import StudentName from './components/StudentName'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name:'App',
+		components:{
+			SchoolName,
+			StudentName
+		}
+	}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
