548a4e76a521de9d425bafc7dacbb9732338fcad
Dixon <510672083@qq.com>
2025-08-06 18:02:29 +0800

    完成

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,110 +1,38 @@
 <template>
-  <div id="root">
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <MyHeader :addTodo="addTodo"/>
-        <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"/>
-        <MyFooter :todos="todos" :checkAllTodo="checkAllTodo"/>
-      </div>
-    </div>
-  </div>
+	<div class="app">
+		<h1>{{msg}}</h1>
+		<School :getSchoolName="getSchoolName"/>
+		<Student v-on:atguigu="getStudentName"/>
+	</div>
 </template>
 
 <script>
-import MyHeader from "./components/MyHeader.vue";
-import MyFooter from "./components/MyFooter.vue";
-import MyList from "./components/MyList.vue";
+	import Student from './components/Student'
+	import School from './components/School'
 
-export default {
-  name: "App",
-  components: {
-    MyHeader,
-    MyFooter,
-    MyList,
-  },
-  data() {
-    return {
-      todos:[
-        {id:'001', title: '抽烟', done: true},
-        {id:'002', title: '喝酒', done: false},
-        {id:'003', title: '开车', done: true},
-      ]
-    }
-  },
-  methods: {
-    // 添加一个todo
-    addTodo(todoObj) {
-      // console.log('我是APP组件，我收到了数据', x);
-      this.todos.unshift(todoObj)
-    },
-    // 勾选or取消勾选一个todo
-    checkTodo(id) {
-      this.todos.forEach((todo) => {
-        if(todo.id === id) todo.done = !todo.done 
-      })
-    },
-    deleteTodo(id) {
-      this.todos = this.todos.filter( todo => todo.id !== id )
-    },
-    // 全选or取消全选
-    checkAllTodo(done) {
-      this.todos.forEach((todo) => {
-        todo.done = done
-      })
-    }
-  }
-};
+	export default {
+		name:'App',
+		components:{School,Student},
+		data() {
+			return {
+				msg: '你好啊！',
+			}
+		},
+		methods: {
+			getSchoolName(name) {
+				console.log('App收到了学校名：', name);
+			},
+			getStudentName(name) {
+				console.log('demo被调用了！', name);
+				
+			}
+		}
+	}
 </script>
 
-<style>
-/*base*/
-body {
-  background: #fff;
-}
-
-.btn {
-  display: inline-block;
-  padding: 4px 12px;
-  margin-bottom: 0;
-  font-size: 14px;
-  line-height: 20px;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
-    0 1px 2px rgba(0, 0, 0, 0.05);
-  border-radius: 4px;
-}
-
-.btn-danger {
-  color: #fff;
-  background-color: #da4f49;
-  border: 1px solid #bd362f;
-}
-
-.btn-danger:hover {
-  color: #fff;
-  background-color: #bd362f;
-}
-
-.btn:focus {
-  outline: none;
-}
-
-.todo-container {
-  width: 600px;
-  margin: 0 auto;
-}
-.todo-container .todo-wrap {
-  padding: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
-
-
-
-
-
-
+<style scoped>
+	.app {
+		background-color: gray;
+		padding: 5px;
+	}
 </style>
