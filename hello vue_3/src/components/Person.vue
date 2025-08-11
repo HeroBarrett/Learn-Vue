9e644c3e26267746fd319f4bfac1f44c6e249e57
Dixon <510672083@qq.com>
2025-08-11 23:11:08 +0800

    路由开始

--- hello vue_3/src/components/Person.vue
+++ hello vue_3/src/components/Person.vue
@@ -1,55 +1,52 @@
 <template>
   <div class="person">
-    <h2>姓名：{{ name }}</h2>
-    <h2>年龄：{{ age }}</h2>
-    <h2>地址：{{ address }}</h2>
+    <h1>情况二：监视【ref】定义的【对象类型】数据</h1>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
-    <button @click="showTel">查看联系方式</button>
+    <button @click="changePerson">修改整个人</button>
   </div>
 </template>
 
-<!-- <script lang="ts">
-export default {
-  name: 'Person',
-}
-</script> -->
-
-<script lang="ts" setup>
-  defineOptions({
-    name: 'Person123'
+<script lang="ts" setup name="Person">
+  import { textChangeRangeNewSpan } from 'typescript';
+  import { ref, watch } from 'vue'
+  let person = ref({
+    name: '张三',
+    age: 18
   })
-  console.log(this) //setup中的this是undefined，Vue3在弱化this了
-  // 数据，原来是写在data中的，此时的name、age、tel都不是响应式的数据
-  let name = '张三'
-  let age = 18
-  let tel = '13888888888'
-  let address = '芜湖'
 
-  // 方法
   function changeName() {
-    name = 'zhang-san' //注意：这样修改name，页面是没有变化的
-    console.log(name) //name确实改了，但name不是响应式的
+    person.value.name += `~`
   }
   function changeAge() {
-    age += 1 //注意：这样修改age，页面是没有变化的
-    console.log(age) //age确实改了，但age不是响应式的
+    person.value.age += 1
   }
-  function showTel() {
-    alert(tel)
+  function changePerson() {
+    person.value = { name: '李四', age: 99 }
   }
+
+  // 监视，情况二：监视【ref】定义的【对象类型】数据
+  watch(person, (newValue, oldValue) => {
+    console.log('person变化了', newValue, oldValue)
+  },{deep:true})
 
 </script>
 
 <style scoped>
-.person {
-  background-color: skyblue;
-  box-shadow: 0 0 10px;
-  border-radius: 10px;
-  padding: 20px;
-}
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
 
-button {
-  margin: 0 5px;
-}
-</style>
+  button {
+    margin: 0 5px;
+  }
+
+  li {
+    font-size: 20px;
+  }
+</style> 
--- hello vue_3/备份/008_reactive创建_对象类型的响应式数据/App.vue
+++ hello vue_3/备份/008_reactive创建_对象类型的响应式数据/App.vue
@@ -9,6 +9,4 @@
     name:'App', //组件名
     components:{Person} //注册组件
   }
-  
-  
 </script>
